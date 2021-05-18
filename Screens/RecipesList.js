80c4bb44965e29496e6e3bed45c7747fa2c7bbfe
ayoubouzzine <ayoubbouzzine@gmail.com>
2021-05-18 09:57:02 +0200

    recipesList_avancé du front

--- Screens/RecipesList.js
+++ Screens/RecipesList.js
@@ -7,48 +7,53 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import styles from '../stylesheets/styles';
 
 function RecipesList(props) {
-    const [searchRecipesList, setSearchRecipesList] = useState('')
+  const [searchRecipesList, setSearchRecipesList] = useState('')
 
-    // fonction de la barre de recherche
-    const updateSearch = (search) => {
-        setSearchRecipesList(search)
-    }
+  // fonction de la barre de recherche
+  const updateSearch = (search) => {
+    setSearchRecipesList(search)
+  }
 
 
-    return (
-        <View >
-            {/* en-tête de page donnant le nom de la page */}
-            <Header
-                centerComponent={{
-                text: 'Recettes',
-                style: styles.headerTitle
-                }}
-                containerStyle={styles.headerContainer}
-            />
+  return (
+    <View>
+      {/* en-tête de page donnant le nom de la page */}
+      <Header
+        centerComponent={{
+        text: 'Recettes',
+        style: styles.headerTitle
+        }}
+        containerStyle={styles.headerContainer}
+        />
 
-            {/* barre de recherche pour les recettes */}
-            <SearchBar
-                round
-                searchIcon = {{size: 24}}
-                lightTheme
-                inputStyle={{backgroundColor: 'white'}}
-                containerStyle={{backgroundColor: 'white', borderWidth: 0}}
-                placeholder = 'Recherche...'
-                onChangeText = {updateSearch}
-                value = {searchRecipesList}
-            />
+        {/* barre de recherche pour les recettes */}
+        <SearchBar
+          round
+          searchIcon = {{size: 24}}
+          lightTheme
+          inputStyle={{backgroundColor: 'white'}}
+          containerStyle={{backgroundColor: 'white', borderWidth: 0}}
+          placeholder = 'Recherche...'
+          onChangeText = {updateSearch}
+          value = {searchRecipesList}
+        />
             
-            <Card>
-            <Card.Title>Recette suggérée</Card.Title>
-            <Card.Divider/>
-            
-            </Card>
+        <Card>
+        <Card.Title>Recette suggérée</Card.Title>
+        <Card.Divider/>
+        <Text style={{marginBottom: 10}}>
+          Pates au pesto
+        </Text>
+        <Card.Image source={require()}>
+        </Card.Image>
+        </Card>
 
-            <Button title = 'recipe'//bouton pour le click sur une recette
-                onPress={()=> props.navigation.navigate('Recipe')}
-            /> 
-        </View>
-    );
+        //bouton pour le click sur une recette
+        <Button title = 'recipe'
+          onPress={()=> props.navigation.navigate('Recipe')}
+        /> 
+    </View>
+  );
 }
 
 export default RecipesList;
