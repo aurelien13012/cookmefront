ef5e842742f052f51511858c4751dbcc2837b84f
Romain Aranda <romain.aranda@gmail.com>
2021-05-19 09:57:48 +0200

    prettier

--- Screens/Account.js
+++ Screens/Account.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
 
-import ConfirmMyRecipe from '../Components/ConfirmMyRecipe'
+import ConfirmMyRecipe from "../Components/ConfirmMyRecipe";
 
 function Account(props) {
- return (
-   <View>   
-        <Text
-          style={{
-            backgroundColor: 'red',
-            marginTop: 50
-          }}
-        >
-          Account
-        </Text>
-        <ConfirmMyRecipe/>
-   </View>
- );
+  return (
+    <View>
+      <Text
+        style={{
+          backgroundColor: "red",
+          marginTop: 50,
+        }}
+      >
+        Account
+      </Text>
+      <ConfirmMyRecipe />
+    </View>
+  );
 }
 
 export default Account;
