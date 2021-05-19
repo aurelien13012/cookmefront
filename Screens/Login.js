7e4c09e5ce72f68346fc4b78bd4a5cadd3a8f3fb
ayoubouzzine <ayoubbouzzine@gmail.com>
2021-05-19 14:27:20 +0200

    Login_front

--- Screens/Login.js
+++ Screens/Login.js
@@ -1,18 +1,148 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useState, useEffect } from "react";
+
+import { View, Text } from "react-native";
+import { Card, Header, Button, Overlay, Input } from "react-native-elements";
+
+
+import styles from "../stylesheets/styles";
 
 function Login(props) {
- return (
-   <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor:'#fff'}}>   
-        <Text>Login</Text>
-        <Button title = 'sign-up' //il faudra faire des redirections pour pointer ou lutilisateur va aller suite à son signup
-        onPress={()=> props.navigation.navigate('My Recipes', {screen : 'My Recipes'})}
-        />
-        <Button title = 'sign-in' //il faudra faire des redirections pour pointer ou lutilisateur va aller suite à son signin
-        onPress={()=> props.navigation.navigate('BottomNavigator', {screen : 'Favorites'})}
-        />
-   </View>
- );
+  const [visible, setVisible] = useState(false);
+  const [signUpUsername, setSignUpUsername] = useState('')
+  const [signUpEmail, setSignUpEmail] = useState('')
+  const [signUpPassword, setSignUpPassword] = useState('')
+
+  const [signInEmail, setSignInEmail] = useState('')
+  const [signInPassword, setSignInPassword] = useState('')
+
+  const [userExists, setUserExists] = useState(false)
+
+  const [listErrorsSignin, setErrorsSignin] = useState([])
+  const [listErrorsSignup, setErrorsSignup] = useState([])
+
+  const toggleOverlay = () => {
+    setVisible(!visible);
+  };
+
+  var handleSubmitSignup = async () => {
+    
+    const data = await fetch('/sign-up', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+      body: `usernameFromFront=${signUpUsername}&emailFromFront=${signUpEmail}&passwordFromFront=${signUpPassword}`
+    })
+
+    const body = await data.json()
+
+    if(body.result == true){
+      props.addToken(body.token)
+      setUserExists(true)
+      
+    } else {
+      setErrorsSignup(body.error)
+    }
+  }
+
+  var handleSubmitSignin = async () => {
+
+    const data = await fetch('/sign-in', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+      body: `emailFromFront=${signInEmail}&passwordFromFront=${signInPassword}`
+    })
+
+    const body = await data.json()
+
+    if(body.result == true){
+      props.addToken(body.token)
+      setUserExists(true)
+      
+    }  else {
+      setErrorsSignin(body.error)
+    }
+  }
+
+  return (
+    <View style = {{flex : 1}}>
+      {/* en-tête de page donnant le nom de la page */}
+      <Header
+        centerComponent={{
+          text: "Login",
+          style: styles.headerTitle,
+        }}
+        containerStyle={styles.headerContainer}
+      />
+
+      <Button title="S'identifier" onPress={toggleOverlay} style={styles.buttonSignIn}/>
+      <Overlay isVisible={visible} onBackdropPress={toggleOverlay} fullScreen={true}>
+        <View style={styles.inputView}>
+          <Input
+            containerStyle={{ marginBottom: 25 }}
+            placeholder="Email"
+            onChangeText={(email) => setEmail(email)}
+          />
+          <Input
+            containerStyle={{ marginBottom: 25 }}
+            placeholder="Password"
+            secureTextEntry={true}
+            onChangeText={(password) => setPassword(password)}
+          />
+
+          <Button
+            title="Valider"
+            buttonStyle={{ backgroundColor: "#FF6F61" }}
+            onPress={() => handleSubmit()}
+            type="solid"
+          />
+        </View>
+      </Overlay>
+      
+      <Button title="S'enregistrer" onPress={toggleOverlay} style={styles.buttonSignUp}/>
+      <Overlay isVisible={visible} onBackdropPress={toggleOverlay} fullScreen={true}>
+        <View style={styles.inputView}>
+          <Input
+            containerStyle={{ marginBottom: 25 }}
+            placeholder="Prénom"
+            onChangeText={(firstName) => setFirstName(firstName)}
+          />
+          <Input
+            containerStyle={{ marginBottom: 25 }}
+            placeholder="Nom"
+            onChangeText={(surName) => setSurName(surName)}
+          />
+          <Input
+            containerStyle={{ marginBottom: 25 }}
+            placeholder="Email"
+            onChangeText={(email) => setEmail(email)}
+          />
+          <Input
+            containerStyle={{ marginBottom: 25 }}
+            placeholder="Password"
+            secureTextEntry={true}
+            onChangeText={(password) => setPassword(password)}
+          />
+
+          <Button
+            title="Valider"
+            buttonStyle={{ backgroundColor: "#FF6F61" }}
+            onPress={() => handleSubmitSignup()}
+            type="solid"
+          />
+        </View>
+      </Overlay>
+  
+    </View>
+
+      // <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor:'#fff'}}>   
+      // <Text>Login</Text>
+      // <Button title = 'sign-up' //il faudra faire des redirections pour pointer ou lutilisateur va aller suite à son signup
+      //   onPress={()=> props.navigation.navigate('My Recipes', {screen : 'My Recipes'})}
+      // />
+      // <Button title = 'sign-in' //il faudra faire des redirections pour pointer ou lutilisateur va aller suite à son signin
+      //   onPress={()=> props.navigation.navigate('BottomNavigator', {screen : 'Favorites'})}
+      // /> 
+    
+  );
 }
 
 export default Login;
--- stylesheets/styles.js
+++ stylesheets/styles.js
@@ -1,183 +1,183 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   headerContainer: {
-    backgroundColor: '#FF6F61',
+    backgroundColor: "#FF6F61",
   },
   headerTitle: {
     fontSize: 26,
-    color: '#fff',
-    fontFamily: 'SourceSansPro_700Bold'
+    color: "#fff",
+    fontFamily: "SourceSansPro_700Bold",
   },
   headerTitleNewRecipe: {
     fontSize: 20,
-    color: '#fff',
-    fontFamily: 'SourceSansPro_700Bold'
+    color: "#fff",
+    fontFamily: "SourceSansPro_700Bold",
   },
   accordionContainer: {
     margin: 20,
-    borderColor: '#FF6F61',
-    borderStyle: 'solid',
+    borderColor: "#FF6F61",
+    borderStyle: "solid",
     borderWidth: 2,
     borderRadius: 10,
-    backgroundColor: 'white'
+    backgroundColor: "white",
   },
   accordionTitle: {
-    color: '#FF6F61',
+    color: "#FF6F61",
     fontSize: 18,
-    fontFamily: 'SourceSansPro_600SemiBold'
+    fontFamily: "SourceSansPro_600SemiBold",
   },
   accordionItemsContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
+    flexDirection: "row",
+    flexWrap: "wrap",
     marginLeft: 20,
     marginRight: 20,
   },
   accordionItem: {
-    backgroundColor: '#fff',
-    color: '#FF6F61',
+    backgroundColor: "#fff",
+    color: "#FF6F61",
     margin: 5,
     borderRadius: 10,
-    borderColor: '#FF6F61',
-    borderStyle: 'solid',
+    borderColor: "#FF6F61",
+    borderStyle: "solid",
     borderWidth: 2,
   },
   accordionItemSelected: {
-    backgroundColor: '#FF6F61',
+    backgroundColor: "#FF6F61",
     margin: 5,
     borderRadius: 10,
-    borderColor: '#FF6F61',
-    borderStyle: 'solid',
+    borderColor: "#FF6F61",
+    borderStyle: "solid",
     borderWidth: 2,
   },
   accordionItemTitle: {
-    color: '#FF6F61',
-    fontFamily: 'SourceSansPro_600SemiBold'
+    color: "#FF6F61",
+    fontFamily: "SourceSansPro_600SemiBold",
   },
   accordionItemTitleSelected: {
-    color: '#fff',
-    fontFamily: 'SourceSansPro_600SemiBold'
+    color: "#fff",
+    fontFamily: "SourceSansPro_600SemiBold",
   },
-  body : {
-    fontFamily : 'SourceSansPro_400Regular_Italic'
+  body: {
+    fontFamily: "SourceSansPro_400Regular_Italic",
   },
-  cardText : {
-    color: '#FF6F61',
-    fontFamily: 'SourceSansPro_600SemiBold',
-    textAlign: 'center',
+  cardText: {
+    color: "#FF6F61",
+    fontFamily: "SourceSansPro_600SemiBold",
+    textAlign: "center",
   },
-  cardImage : {
-    height : 100, 
-    width : '80%',
-    marginLeft: 30 
+  cardImage: {
+    height: 100,
+    width: "80%",
+    marginLeft: 30,
   },
-  image : {
-    height : 20, 
-    width : '20%',
+  image: {
+    height: 20,
+    width: "20%",
   },
-  cardName : {
-    color: '#FF6F61',
-    fontFamily: 'SourceSansPro_600SemiBold',
+  cardName: {
+    color: "#FF6F61",
+    fontFamily: "SourceSansPro_600SemiBold",
   },
-  cardLigne : {
-    display : 'flex',
-    flexDirection : 'row',
-    justifyContent : 'space-between',
-    marginTop : 10,
+  cardLigne: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 10,
   },
-  itemMyRecipes : {
-    backgroundColor: '#fff',
-    color: '#FF6F61',
+  itemMyRecipes: {
+    backgroundColor: "#fff",
+    color: "#FF6F61",
     margin: 5,
     borderRadius: 10,
-    borderColor: '#FF6F61',
-    borderStyle: 'solid',
+    borderColor: "#FF6F61",
+    borderStyle: "solid",
     borderWidth: 2,
   },
-  itemMyRecipesTitle : {
-      color : '#FF6F61',
-      fontFamily : 'SourceSansPro_600SemiBold'
+  itemMyRecipesTitle: {
+    color: "#FF6F61",
+    fontFamily: "SourceSansPro_600SemiBold",
   },
-  noRecipes : {
-    color : '#FF6F61',
-    fontFamily : 'SourceSansPro_600SemiBold',
-    margin : 15,
-    textAlign : 'center'
+  noRecipes: {
+    color: "#FF6F61",
+    fontFamily: "SourceSansPro_600SemiBold",
+    margin: 15,
+    textAlign: "center",
   },
-  addRecipe : {
-    backgroundColor: '#FF6F61',
-    color: '#FF6F61',
+  addRecipe: {
+    backgroundColor: "#FF6F61",
+    color: "#FF6F61",
     marginTop: 100,
-    marginLeft : 125,
+    marginLeft: 125,
     borderRadius: 50,
-    width : 100,
-    height : 100,
-    borderColor: '#FF6F61',
-    borderStyle: 'solid',
+    width: 100,
+    height: 100,
+    borderColor: "#FF6F61",
+    borderStyle: "solid",
     borderWidth: 2,
-    padding : 20
+    padding: 20,
   },
-  addRecipeTitle : {
-    color: '#fff',
-    fontFamily: 'SourceSansPro_600SemiBold',
-    fontSize : 40
+  addRecipeTitle: {
+    color: "#fff",
+    fontFamily: "SourceSansPro_600SemiBold",
+    fontSize: 40,
   },
-  addNew : {
-    backgroundColor: '#FF6F61',
-    color: '#FF6F61',
+  addNew: {
+    backgroundColor: "#FF6F61",
+    color: "#FF6F61",
     marginTop: 15,
     // marginLeft : 125,
     borderRadius: 50,
-    width : 20,
-    height : 20,
-    borderColor: '#FF6F61',
-    borderStyle: 'solid',
+    width: 20,
+    height: 20,
+    borderColor: "#FF6F61",
+    borderStyle: "solid",
     borderWidth: 2,
-    padding : 2
+    padding: 2,
   },
-  addNewRecipeTitle : {
-    color: '#fff',
-    fontFamily: 'SourceSansPro_600SemiBold',
-    fontSize : 15
+  addNewRecipeTitle: {
+    color: "#fff",
+    fontFamily: "SourceSansPro_600SemiBold",
+    fontSize: 15,
   },
   searchBar: {
-    color: '#FF6F61'
+    color: "#FF6F61",
   },
   NewRecipeContainer: {
-    display : 'flex',
-    flexDirection: 'row',
+    display: "flex",
+    flexDirection: "row",
     marginLeft: 20,
     marginRight: 20,
-    marginTop : 10 
+    marginTop: 10,
   },
   NewResultContainer: {
-    display : 'flex',
+    display: "flex",
     marginLeft: 20,
     marginRight: 20,
-    marginTop : 10 
+    marginTop: 10,
   },
-  body : {
-    fontFamily: 'SourceSansPro_400Regular',
-    fontSize: 20
+  body: {
+    fontFamily: "SourceSansPro_400Regular",
+    fontSize: 20,
   },
   recipePic: {
-    width: '100%', 
-    height: 300
+    width: "100%",
+    height: 300,
   },
-  infoBoxContainer:{
-    width: '80%',
+  infoBoxContainer: {
+    width: "80%",
     // height: 100,
-    borderColor: '#FF6F61',
-    borderStyle: 'solid',
+    borderColor: "#FF6F61",
+    borderStyle: "solid",
     borderWidth: 2,
     borderRadius: 10,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     // position: 'absolute',
     // top: 0,
     // left: 40,
@@ -196,15 +196,36 @@ const styles = StyleSheet.create({
   },
   infoBoxTitle: {
     fontSize: 26,
-    color: '#FF6F61',
-    fontFamily: 'SourceSansPro_700Bold',
+    color: "#FF6F61",
+    fontFamily: "SourceSansPro_700Bold",
     marginLeft: 10,
-    width: '80%',
+    width: "80%",
   },
   recipeMiniPic: {
     width: 150,
     height: 100,
-    margin: 5
+    margin: 5,
+  },
+  inputView: {
+    width: "80%",
+    height: "50%",
+    marginTop: 120,
+    marginLeft: 20,
+    alignItems: "center",
+  },
+  buttonSignIn: {
+    borderRadius: 25,
+    height: 60,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 120,
+  },
+  buttonSignUp: {
+    borderRadius: 25,
+    height: 60,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
   }
 });
 
